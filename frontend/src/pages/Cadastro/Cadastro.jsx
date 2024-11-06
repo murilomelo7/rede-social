6810dc3816da06e47e16a315c5894ef140351319
murilomelo7 <95890358+murilomelo7@users.noreply.github.com>
2024-11-06 12:37:40 -0300

    feature

--- frontend/src/pages/Cadastro/Cadastro.jsx
+++ frontend/src/pages/Cadastro/Cadastro.jsx
@@ -1,11 +1,13 @@
-
+import { Box } from "@mui/material";
 
  function Cadastro(){
   return (
+    <Box sx={{ height: '100vh',           
+      backgroundImage: 'linear-gradient(to left, #4a148c, #9b59b6)',
+      display: 'flex' }}>
 
-    <div>
-      a
-    </div>
+
+        </Box>
   );
 }
 
--- frontend/src/pages/Inicial/Inicial.jsx
+++ frontend/src/pages/Inicial/Inicial.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '@mui/material/Input/Input';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
-import { Stack, Typography } from '@mui/material';
+import { Stack, TextField, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 function Inicial() {
   const navigate = useNavigate();
 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const handleLoginClick = () => {
     navigate('/login');
   }
@@ -17,36 +19,77 @@ function Inicial() {
     navigate('/cadastro');
   }
 
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (username && password) {
+      console.log('Usuário:', username);
+      console.log('Senha:', password);
+      navigate('/home');
+    }
+  };
+
   return (
-    <Box sx={{ height: '100vh', display: 'flex' }}>
-      {/* Lado esquerdo - Logo */}
+    <Box sx={{ height: '100vh',
+       
+      display: 'flex' }}>
       <Box 
         sx={{ 
           flex: 1, 
-          backgroundColor: 'background.default', // Usa a cor de fundo do tema
           display: 'flex', 
           alignItems: 'center', 
-          justifyContent: 'center' 
+          justifyContent: 'center' ,
+          backgroundImage: 'linear-gradient(to left, #121212, #5f258e)',
+
+          
         }}
       >
         <Typography variant="h3" component="div" sx={{ fontWeight: 'bold' }}>
-         M
+          M
         </Typography>
       </Box>
       <Box 
         sx={{ 
-          flex: 1, 
+          flex: 0.8, 
           backgroundColor: 'background.paper',
           display: 'flex', 
           alignItems: 'center', 
-          justifyContent: 'center' 
+          justifyContent: 'center',
+          borderTopLeftRadius: '6px',
+          borderBottomLeftRadius: '6px',
+          // backgroundImage: 'linear-gradient(to left, #121212, #5f258e)',
+
+        
         }}
       >
         <Stack spacing={2} sx={{ width: '60%' }}>
-          <Button variant="contained" color="primary" onClick={handleLoginClick}>
-            Login
-          </Button>
-          <Button variant="outlined" color="primary">
+        <Typography variant="h4" align="center" style={{ marginBottom: '30px' }}>Login</Typography>
+
+        <form onSubmit={handleSubmit} autoComplete='off' style={{ display: 'flex', flexDirection: 'column' }}>
+              <TextField 
+                label="Usuário" 
+                variant="outlined" 
+                fullWidth
+                autoComplete='off'
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+                required
+              />
+              <TextField style={{ marginTop: '15px' }}
+                label="Senha" 
+                variant="outlined" 
+                type="password" 
+                fullWidth 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                required
+              />
+              <Button variant="contained" color="primary" style={{ marginTop: '20px' }} type="submit">
+                Entrar
+              </Button>
+            </form>
+          <Button variant="outlined" color="primary" onClick={handleCadastroClick}>
             Cadastrar
           </Button>
         </Stack>
